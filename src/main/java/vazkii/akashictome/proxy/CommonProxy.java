76aafc8c5abe706310697473c8cd0d92a46c6e5a
Vasco Lavos <vazkii@hotmail.com>
2016-09-02 02:27:43 +0100

    woah dude a recipe nice

--- src/main/java/vazkii/akashictome/proxy/CommonProxy.java
+++ src/main/java/vazkii/akashictome/proxy/CommonProxy.java
@@ -1,33 +1,26 @@
 package vazkii.akashictome.proxy;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.fml.relauncher.Side;
-import vazkii.akashictome.ConfigHandler;
-import vazkii.akashictome.ItemTome;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import vazkii.akashictome.ModItems;
 import vazkii.akashictome.MorphingHandler;
 import vazkii.akashictome.WikiFallback;
 import vazkii.akashictome.network.MessageRegister;
-import vazkii.akashictome.network.message.MessageMorphTome;
-import vazkii.arl.network.NetworkHandler;
-import vazkii.botania.api.wiki.WikiHooks;
 
 public class CommonProxy {
 
 	public void preInit() {
 		ModItems.init();
 		
-//		GameRegistry.addShapedRecipe(new ItemStack(tome),
-//				" GB", " IR", "I  ",
-//				'G', new ItemStack(Items.DYE, 1, EnumDyeColor.GREEN.getDyeDamage()),
-//				'B', new ItemStack(Items.DYE, 1, EnumDyeColor.BLUE.getDyeDamage()),
-//				'R', new ItemStack(Items.DYE, 1, EnumDyeColor.RED.getDyeDamage()),
-//				'I', new ItemStack(Items.IRON_INGOT)); TODO
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tome), new ItemStack(Items.BOOK), new ItemStack(Blocks.BOOKSHELF));
 		
 		MinecraftForge.EVENT_BUS.register(MorphingHandler.INSTANCE);
 		initHUD();
