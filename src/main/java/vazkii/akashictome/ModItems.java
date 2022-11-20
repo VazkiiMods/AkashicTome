d64fccf6abf95cb29d76c0c8ee76dbe99123f963
Vasco Lavos <vazkii@hotmail.com>
2022-11-20 13:47:08 +0000

    build

--- src/main/java/vazkii/akashictome/ModItems.java
+++ src/main/java/vazkii/akashictome/ModItems.java
@@ -7,19 +7,20 @@ import net.minecraft.world.item.crafting.SimpleRecipeSerializer;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.RegisterEvent;
+
 import vazkii.arl.util.RegistryHelper;
 
 public final class ModItems {
 
 	public static final RecipeSerializer<AttachementRecipe> ATTACHMENT = new SimpleRecipeSerializer<>(AttachementRecipe::new);
-	
+
 	public static Item tome;
 
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public static void onRegistryInit(RegisterEvent event) {
 		if (tome == null) {
 			tome = new TomeItem();
-			
+
 			RegistryHelper.register(ATTACHMENT, AkashicTome.MOD_ID + ":attachment", Registry.RECIPE_SERIALIZER_REGISTRY);
 		}
 	}
