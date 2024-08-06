91d94a6d5254be922d0fe37a78b75277fef0f286
Uraneptus <uraneptus33@gmail.com>
2024-08-06 03:41:11 +0200

    fixed log spam

--- src/main/java/vazkii/akashictome/client/HUDHandler.java
+++ src/main/java/vazkii/akashictome/client/HUDHandler.java
@@ -7,6 +7,8 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
@@ -56,20 +58,21 @@ public class HUDHandler {
 
 				String mod = MorphingHandler.getModFromState(state);
 				ItemStack morphStack = MorphingHandler.getShiftStackForMod(tomeStack, mod);
-				if (!morphStack.isEmpty() && !ItemStack.isSameItemSameTags(morphStack, tomeStack)) { //TODO test if same tags as well
+				if (!morphStack.isEmpty() && !ItemStack.isSameItemSameTags(morphStack, tomeStack)) {
 					drawStack = morphStack;
 					line1 = NBTUtils.getCompound(morphStack, MorphingHandler.TAG_TOME_DISPLAY_NAME, false).getString("text");
 					line2 = ChatFormatting.GRAY + I18n.get("akashictome.click_morph");
 				}
+				MutableComponent line1Component = Component.Serializer.fromJson(line1);
 
-				if (!drawStack.isEmpty()) {
+				if (!drawStack.isEmpty() && line1Component != null) {
 					RenderSystem.enableBlend();
 					RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 					int sx = res.getGuiScaledWidth() / 2 - 17;
 					int sy = res.getGuiScaledHeight() / 2 + 2;
 
 					guiGraphics.renderItem(drawStack, sx, sy);
-					guiGraphics.drawString(mc.font, line1, sx + 20, sy + 4, 0xFFFFFFFF);
+					guiGraphics.drawString(mc.font, line1Component.withStyle(ChatFormatting.GREEN), sx + 20, sy + 4, 0xFFFFFFFF);
 					guiGraphics.drawString(mc.font, line2, sx + 25, sy + 14, 0xFFFFFFFF);
 				}
 			}
